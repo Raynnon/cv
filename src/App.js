abe31a32ea9b26fee39da81e98c84c87a1f8d53e
Raynnon <assante.f@gmail.com>
2020-08-30 01:14:47 +0200

    recreated the header + about me section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import Header from './components/Header';
+
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+
 import './custom.scss';
 import './app.scss';
-import './styles/header.css'
-import Container from 'react-bootstrap/Container'
+
+
 
 function App() {
   return (
-    <Container className="App bg-light" fluid>
-      <Header />
+    /* Header */
+    <Container fluid>
+      <Container id="header" as="header" fluid>
+        <h2>Hello, I am <span id="me">Florian Assante</span></h2>
+        <h1>The self-made Web Developer.</h1>
+        <Button class="header-button" variant="secondary" size="lg">Contact me</Button>
+        <Button class="header-button" variant="outline-light" size="lg">Download CV</Button>
+      </Container>
+
+      <Container id="about" as="section">
+        <h3>About me</h3>
+        <Row>
+          <Col>
+            <Image id="me" src={require("./images/florian-assante.png")} fluid rounded />
+          </Col>
+          <Col>
+            <p id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia erat id nisl efficitur 
+condimentum. Nullam ac nisi at mauris dignissim tristique eu et justo. Nam maximus 
+efficitur massa, eget mattis quam pellentesque ac. Etiam tincidunt congue fringilla. Nullam q
+uis lectus sapien. Praesent et mollis orci. Suspendisse et eros sapien. In eget dapibus lorem. 
+Nunc in tortor dignissim, bibendum nibh a, porta risus.</p>
+            <Row>
+              <p class="hobbies"><i class="fas fa-globe-europe"></i> French</p>
+              <p class="hobbies"><i class="fas fa-home"></i> Spain</p>
+              <p class="hobbies"><i class="fas fa-language"></i> French, Spanish, English</p>
+            </Row>
+            <Row>
+              <p class="hobbies"><i class="fas fa-plane-departure"></i> Travel</p>
+              <p class="hobbies"><i class="fas fa-table-tennis"></i> Sports</p>
+              <p class="hobbies"><i class="fas fa-gamepad"></i> Video Games</p>
+              <p class="hobbies"><i class="fas fa-book-open"></i> Learning</p>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
