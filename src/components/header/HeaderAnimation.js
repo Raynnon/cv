71e69bfc911c2f349bb94ae351fcbe810a25c9a1
Raynnon <assante.f@gmail.com>
2021-02-03 23:33:14 +0100

    change images origin of the waves

--- src/components/header/HeaderAnimation.js
+++ src/components/header/HeaderAnimation.js
@@ -1,19 +1,19 @@
-// @flow 
-import * as React from 'react';
-import './Animation.css';
+// @flow
+import * as React from "react";
+import "./Animation.css";
 
 export const HeaderAnimation = () => {
-    return (
-        <div id="wave" className="waveWrapper waveAnimation">
-            <div className="waveWrapperInner bgTop">
-                <div className="wave waveTop" style={{backgroundImage: `url('http://front-end-noobs.com/jecko/img/wave-top.png')`}}></div>
-            </div>
-            <div className="waveWrapperInner bgMiddle">
-                <div className="wave waveMiddle" style={{backgroundImage: `url('http://front-end-noobs.com/jecko/img/wave-mid.png')`}}></div>
-            </div>
-            <div className="waveWrapperInner bgBottom">
-                <div className="wave waveBottom" style={{backgroundImage: `url('http://front-end-noobs.com/jecko/img/wave-bot.png')`}}></div>
-            </div>
-        </div>
-    );
+  return (
+    <div id="wave" className="waveWrapper waveAnimation">
+      <div className="waveWrapperInner bgTop">
+        <div className="wave waveTop"></div>
+      </div>
+      <div className="waveWrapperInner bgMiddle">
+        <div className="wave waveMiddle"></div>
+      </div>
+      <div className="waveWrapperInner bgBottom">
+        <div className="wave waveBottom"></div>
+      </div>
+    </div>
+  );
 };
