00f8d4b58ef2736522ebe4a5c27da39fb69b4e9e
Raynnon <assante.f@gmail.com>
2021-02-06 23:30:34 +0100

    rewrote app.js to use hooks

--- src/components/header/HeaderModal.js
+++ src/components/header/HeaderModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import emailjs from "emailjs-com";
 
@@ -6,131 +6,49 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-export default class HeaderModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      company: "",
-      email: "",
-      message: "",
-      emailSent: "",
-      recaptcha: 0,
-    };
+export default function HeaderModal(props) {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState("");
+  const [recaptcha, setRecaptcha] = useState(0);
 
-    this.email = this.Email.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleRecaptchaChange = this.handleRecaptchaChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  Email() {
-    if (!this.state.emailSent) {
-      return (
-        <Modal
-          {...this.props}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Contact me
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  required
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="company">
-                <Form.Label>Company</Form.Label>
-                <Form.Control type="text" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group controlId="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  required
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-
-              <Form.Group controlId="text-area">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={5}
-                  required
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="text-area">
-                <ReCAPTCHA
-                  sitekey="6LcPHD8aAAAAAF3hH5M-ElCEVdFcjVe_gYnRcpxH"
-                  onChange={this.handleRecaptchaChange}
-                />
-              </Form.Group>
-              <Button
-                className="cta-button float-right mr-0"
-                variant="secondary"
-                type="submit"
-              >
-                Send
-              </Button>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      );
-    } else {
-      return (
-        <Modal
-          {...this.props}
-          size="md"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-        >
-          <Modal.Header className="border-bottom-0" closeButton>
-            <p className="text-success mx-auto my-auto">Email Sent</p>
-          </Modal.Header>
-        </Modal>
-      );
-    }
-  }
-
-  handleChange(event) {
+  const handleChange = (event) => {
     if (event.target.id === "text-area") {
-      this.setState({ message: event.target.value });
+      setMessage({ message: event.target.value });
     } else {
       const obj = {};
       const key = event.target.id;
       obj[key] = event.target.value;
 
-      this.setState(obj);
+      switch (event.target.id) {
+        case "name":
+          setName(obj[key]);
+          break;
+        case "company":
+          setCompany(obj[key]);
+          break;
+        case "email":
+          setEmail(obj[key]);
+          break;
+        default:
+          break;
+      }
     }
-  }
+  };
 
-  handleRecaptchaChange() {
-    this.setState({ recaptcha: 1 });
-  }
-
-  handleSubmit() {
-    if (this.state.recaptcha) {
-      this.setState({ emailSent: true });
-      const { name, company, email, message } = this.state;
+  const handleSubmit = () => {
+    if (recaptcha) {
+      setEmailSent({ emailSent: true });
       const templateParams = {
         name,
         company,
         email,
         message,
       };
+
+      console.log(templateParams);
 
       const serviceId = process.env.REACT_APP_SERVICEID;
       const templateId = process.env.REACT_APP_TEMPLATEID;
@@ -141,15 +59,75 @@ export default class HeaderModal extends Component {
           console.log("SUCCESS!", response.status, response.text);
         },
         (error) => {
-          this.setState({ emailSent: false });
+          setEmailSent({ emailSent: false });
           console.log("FAILED...", error);
         }
       );
     }
-  }
+  };
 
-  render() {
-    const Email = this.email;
-    return <Email />;
-  }
+  return !emailSent ? (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Contact me</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" required onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="company">
+            <Form.Label>Company</Form.Label>
+            <Form.Control type="text" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" required onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group controlId="text-area">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              required
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="text-area">
+            <ReCAPTCHA
+              sitekey="6LcPHD8aAAAAAF3hH5M-ElCEVdFcjVe_gYnRcpxH"
+              onChange={() => setRecaptcha({ recaptcha: 1 })}
+            />
+          </Form.Group>
+          <Button
+            className="cta-button float-right mr-0"
+            variant="secondary"
+            type="submit"
+          >
+            Send
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  ) : (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header className="border-bottom-0" closeButton>
+        <p className="text-success mx-auto my-auto">Email Sent</p>
+      </Modal.Header>
+    </Modal>
+  );
 }
