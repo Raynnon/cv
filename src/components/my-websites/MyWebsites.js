1202a7462f8a1ad1222620905d9c3b6336c703a3
Raynnon <assante.f@gmail.com>
2021-11-07 17:58:20 +0100

    added mes cadeaux originaux in my websites

--- src/components/my-websites/MyWebsites.js
+++ src/components/my-websites/MyWebsites.js
@@ -6,18 +6,28 @@ import Container from "react-bootstrap/Container";
 import organisemeImage from "./images/organise-me.jpg";
 import cvImage from "./images/cv.jpg";
 import weatherImage from "./images/weather-app.jpg";
+import cadeauxOriginauxImage from "./images/mes-cadeaux-originaux.png";
 
 const MyWebsites = () => {
   return (
     <Container id="my-websites" className="pr-0 pl-0" as="section" fluid>
       <h2>My Websites</h2>
       <ItemContent
+        name="Mes cadeaux originaux (11/2021)"
+        imageAlt="mes-cadeaux-originaux"
+        liveLink="https://mescadeauxoriginaux.fr"
+        sourceLink="https://github.com/Raynnon/cadeaux-originaux"
+        description="Affiliate site made with Next.js, Express.js and mongoDB"
+        image={cadeauxOriginauxImage}
+      />
+
+      <ItemContent
         name="Organise Me (04/2021)"
         imageAlt="organise-me"
         liveLink="https://organiseme.florian-assante.com"
         sourceLinkFront="https://github.com/Raynnon/organiseme"
         sourceLinkBack="https://github.com/Raynnon/tasks-api"
-        description="A task manager created with React, Express and MongoDB"
+        description="A task manager created with React, Express.js and MongoDB"
         image={organisemeImage}
       />
 
@@ -26,7 +36,7 @@ const MyWebsites = () => {
         imageAlt="weather-app"
         liveLink="https://weather.florian-assante.com"
         sourceLink="https://github.com/Raynnon/weather-app"
-        description="A weather app created with EJS and Express using the Weatherbit API."
+        description="A weather app created with EJS and Express.js using the Weatherbit API."
         image={weatherImage}
       />
 
